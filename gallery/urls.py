39af3c39058c4c11a20a25a1389d821951d95bfd
APT-KK <khoslakabeer527@gmail.com>
2025-12-14 23:44:48 +0530

    make urls for gallery viewsets

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,1 +1,13 @@
-from django.urls import path
+from django.urls import path, include
+from .views import PhotoViewSet, AlbumViewSet, EventViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+
+router.register(r'photos', PhotoViewSet)
+router.register(r'albums', AlbumViewSet)
+router.register(r'events', EventViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
