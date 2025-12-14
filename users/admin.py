07139eb078570b21be35d79df5096320d5ab9b96
APT-KK <khoslakabeer527@gmail.com>
2025-12-14 11:46:07 +0530

    register user in admin.py to display in admin panel

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+
+    list_display = ['email', 'full_name', 'role', 'is_verified', 'is_staff']
+    list_filter = ['role', 'is_verified', 'is_staff', 'is_superuser']
+    # fieldsets means what admin can change in the user form
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal Info', {'fields': ('full_name', 'profile_picture', 'bio')}),
+        ('Permissions', {'fields': ('role', 'is_verified', 'is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    
+    # after add user what happens 
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password', 'confirm_password'), 
+        }),
+    )
+
+    search_fields = ['email', 'full_name']
+    ordering = ['email']
+
+admin.site.register(CustomUser, CustomUserAdmin)
