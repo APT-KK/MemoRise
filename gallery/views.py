d5fea288cd28e3a71e7de12807ad0c258776bf07
APT-KK <khoslakabeer527@gmail.com>
2025-12-14 23:37:50 +0530

    make gallery views

--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from .models import Photo, Album, Event
+from .serializers import PhotoSerializer, AlbumSerializer, EventSerializer
+from rest_framework import viewsets, permissions, parsers
 
-# Create your views here.
+# we use ViewSets as we need CRUD op for these models
+
+class PhotoViewSet(viewsets.ModelViewSet):
+    queryset = Photo.objects.all().order_by('-uploaded_at')
+    serializer_class = PhotoSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    parser_classes = [
+        parsers.MultiPartParser,
+        parsers.FormParser
+    ]
+
+    def perform_create(self, serializer):
+        serializer.save(photographer=self.request.user)
+
+class AlbumViewSet(viewsets.ModelViewSet):
+    queryset = Album.objects.all().order_by('-created_at')
+    serializer_class = AlbumSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+class EventViewSet(viewsets.ModelViewSet):
+    queryset = Event.objects.all().order_by('-date')
+    serializer_class = EventSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(coordinator=self.request.user)
