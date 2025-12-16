7b2f0e8f4c43ea79db7940b5ade61d2202ed364f
APT-KK <khoslakabeer527@gmail.com>
2025-12-16 12:39:12 +0530

    fix firebase path

--- users/apps.py
+++ users/apps.py
@@ -1,14 +1,16 @@
-from django.apps import AppConfig
 import firebase_admin
+import os
+from django.apps import AppConfig
 from firebase_admin import credentials
 
 class UsersConfig(AppConfig):
     name = 'users'
 
     def ready(self):
+
         if not firebase_admin._apps:
             try:
-                cred_path = ('firebase_config.json')
+                cred_path = os.path.join('users', 'firebase_config.json')
                 cred = credentials.Certificate(cred_path)
                 firebase_admin.initialize_app(cred)
             except Exception as e:
